d883d47458e4614c6ddd3545ce64934ae98c07b6
JackHolland <sirjackholland@gmail.com>
2013-02-20 23:14:01 -0500

    Homework 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,44 @@
 
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    sortby = params[:sort]
+    if sortby
+    	sort = sortby
+    	session[:sort] = sort
+    	if sortby == 'title'
+    		@title_header_class = 'hilite'
+    	elsif sortby == 'rating'
+    		@rating_header_class = 'hilite'
+    	elsif sortby == 'release_date'
+    		@release_date_header_class = 'hilite'
+    	end
+    elsif session[:sort]
+    	sort = session[:sort]
+    	redirect = true
+    else
+    	sort = 'id'
+    end
+    ratings_allowed = params[:ratings]
+    if ratings_allowed
+    	if ratings_allowed.respond_to?(:keys)
+    		ratings = ratings_allowed.keys
+    	else
+    		ratings = ratings_allowed
+    	end
+    	session[:ratings] = ratings
+    elsif session[:ratings]
+    	ratings = session[:ratings]
+    	redirect = true
+    else
+    	ratings = Movie.ratings
+    end
+    if redirect
+    	flash.keep
+    	redirect_to movies_path(:sort => sort, :ratings => ratings)
+    else
+    	@movies = Movie.where(:rating => ratings).order(sort)
+    	@all_ratings = Movie.ratings
+    end
   end
 
   def show
